fc7b00578dfd26532184ea338873cb72a04d437e
xsx1t <froggyuchia@gmail.com>
2026-04-14 10:47:34 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+var myApp = angular.module('myApp', ['youtube-embed']);
 
+myApp.controller('MyCtrl', function($scope) {
+  // Your video ID from https://youtu.be/s8rI4D_Kuqw
+  $scope.videoId = 's8rI4D_Kuqw';
+
+  // Optional: Loop the video when it ends
+  $scope.$on('youtube.player.ended', function() {
+    $scope.player.playVideo();
+  });
+});   
